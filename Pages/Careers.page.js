29b3da68d22897fc7bb2fec5790d8fbccaa6c8ee
batuhanaybak <48447901+batuhanaybak@users.noreply.github.com>
2019-04-23 20:18:25 -0400

    Update Careers.page.js

--- Pages/Careers.page.js
+++ Pages/Careers.page.js
@@ -1,9 +1,12 @@
 var Careers = function () {
-    
+
     this.officeLocations = $('.careers-locations h2')
     this.locations = $$('.careers-locations h4');
     this.expectedLocations = [ 'Cincinnati, OH', 'Arlington, VA', 'Remote' ];
+    this.virginia = $('.bg-purple a');
 
+                                
+    
 };
 
 module.exports = new Careers();
